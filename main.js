d0c754fd75dbb5408603874d0316100cb3f58600
ulysse <juget.ulysse@gmail.com>
2023-07-06 19:04:16 +0200

    added a to do list

--- main.js
+++ main.js
@@ -3,7 +3,14 @@
 //Installer le package commander pour créer des commandes simples et avec des options. 
 //trouver des parametres : -a --author pour les auteurs de la quote. -n --number pour le nombre de quote.
 
-// Source quotes : https://github.com/sin0light/api-kaamelott/blob/master/assets/quotes.json
+//  Features : 
+//  -a --author pour les auteurs de la quote.
+//  -b --book pour le livre de la quote.
+//  -c --character pour le personnage de la quote.
+// -reload la liste des quotes. (scraper le wikifandom ? https://kaamelott.fandom.com/fr/wiki/Citations)
+// - -h --help pour afficher l'aide.
+// - -v --version pour afficher la version.
+// ajouter un mode pour créer des citation avec photo de fond. (bibliotheque de photo ? )
 
 const quotes = require('./src/assets/quotes.json');
 
